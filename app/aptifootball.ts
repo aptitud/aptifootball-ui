d4a02c16b0cc2b0b83e3879fa68f14b9bd0d5b30
tgranbacka <tgranbacka@gmail.com>
2015-11-20 10:49:25 +0100

    First commit

---
+++ app/aptifootball.ts
@@ -0,0 +1,7 @@
+import {bootstrap, Component} from 'angular2/angular2';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Aptifootball</h1>'
+})
+class AppComponent { }
+bootstrap(AppComponent);
